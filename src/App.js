71effb9f6b966ea57c5c99244db7c030133ef89e
Rahul Yadav <rahulkyadav9691@gmail.com>
2022-01-26 23:30:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route,Routes} from 'react-router-dom';
+
+//components 
+import About from './About';
+import Contact from './Contact';
+import Main from './Main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Routes>
+       <Route  path = "/" element = {<Main/>} />
+       <Route path = "about" element = {<About/>} />
+       <Route path = "contact" element = {<Contact/>} />
+     </Routes>
   );
 }
 
---
+++ src/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Main = () => {
+    return(
+        <h1>
+            This is a basic React Router tutorial 2022
+        </h1>
+    )
+}
+export default Main;
